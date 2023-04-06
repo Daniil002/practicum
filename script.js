f3cbd899cbbecc7c06c4585e23772260300a8377
Daniil002 <prokopenkodaniil789@gmail.com>
2023-04-06 10:48:48 +0200

    commit message

--- script.js
+++ script.js
@@ -23,29 +23,41 @@
 
 'use strict';
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-
-
-personalMovieDB = {
+const personalMovieDB = {
     count: numberOfFilms,
-    movies: {}, 
+    movies: {},
     actors: {},
     genres: [],
     privat: false
 };
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
 
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
 
-console.log(personalMovieDB)
 
 
 
